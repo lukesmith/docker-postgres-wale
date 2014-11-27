6d099f890d440e949797f80795eb6e7cdfe3e7bb
lukesmith <stuff@lukesmith.net>
2014-11-27 22:35:41 +0000

    cron job to delete (retain 7) backups

--- setup-wale.sh
+++ setup-wale.sh
@@ -28,6 +28,7 @@ else
             echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
 
             su - postgres -c "crontab -l | { cat; echo \"0 3 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push /var/lib/postgresql/data\"; } | crontab -"
+            su - postgres -c "crontab -l | { cat; echo \"0 4 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e delete --confirm retain 7\"; } | crontab -"
         fi
     fi
 fi
